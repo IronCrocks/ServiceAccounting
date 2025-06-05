eba3d1f868e4fc80bae8e783f50937c593c1a568
Kirill <kirillsuhotchenko@yandex.ru>
2025-06-05 17:39:39 +0300

    Изменил директорию для БД

--- Model/ApplicationDBContext.cs
+++ Model/ApplicationDBContext.cs
@@ -14,40 +14,11 @@ public class ApplicationDBContext : DbContext
     {
         //optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;" +
         //    "Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
-
-        optionsBuilder.UseSqlite("Data Source=lite1.db");
+        
+        var path = Path.Combine(AppContext.BaseDirectory, "lite1.db");
+        optionsBuilder.UseSqlite($"Data Source={path}");
+        
         //optionsBuilder.UseSqlServer("Data Source=DESKTOP-Q9PBDCE;Integrated Security=True;Connect Timeout=30;" +
         //    "Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        //modelBuilder
-        //        .Entity<Order>()
-        //        .HasMany(c => c.Products)
-        //        .WithMany(s => s.Orders)
-        //        .UsingEntity<OrderItem>(
-        //           j => j
-        //            .HasOne(pt => pt.Product)
-        //            .WithMany(t => t.OrderItems)
-        //            .HasForeignKey(pt => pt.ProductId),
-        //        j => j
-        //            .HasOne(pt => pt.Order)
-        //            .WithMany(p => p.OrderItems)
-        //            .HasForeignKey(pt => pt.OrderId),
-        //        j =>
-        //        {
-        //            j.Property(pt => pt.Count).HasDefaultValue(1);
-        //            j.HasKey(t => t.Id);
-        //            j.ToTable("OrderItems");
-        //        });
-
-        //j.Property(pt => pt.Date).HasDefaultValueSql("CURRENT_TIMESTAMP");
-
-        //modelBuilder.Entity<CompanyProductsGroup>(pc =>
-        //{
-        //    pc.HasNoKey();
-        //    pc.ToView("CustomersTotalSumView");
-        //});
     }
 }
